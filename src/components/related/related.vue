5bab3f825ccdfddc29fd51b0c1a74c1b339785d8
Mario X Wang <wangxing_188@126.com>
2019-01-19 18:39:46 +0800

    修改组件宽

--- src/components/related/related.vue
+++ src/components/related/related.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="related">
+  <div>
     <div class="title">Related products</div>
     <ul class="list">
       <li class="item">
@@ -19,31 +19,27 @@ export default {
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
-.related {
-  width: 14.2857rem;
-
-  .title {
-    font-size: 1.2857rem;
-    font-weight: 700;
-    line-height: 2.5rem;
-    color: #fefefe;
-    background: #949494;
-    text-align: center;
-  }
-  .list {
-    background: #f5f5f5;
-    min-height: 35.7143rem;
-    .item {
-      padding: 1.4286rem 1.0714rem;
-      > div {
-        width: 100%;
-        height: 12.1429rem;
-        border: 1px solid #000;
-      }
-      > p {
-        font-size: 1.1429rem;
-        line-height: 1.4286rem;
-      }
+.title {
+  font-size: 1.2857rem;
+  font-weight: 700;
+  line-height: 2.5rem;
+  color: #fefefe;
+  background: #949494;
+  text-align: center;
+}
+.list {
+  background: #f5f5f5;
+  min-height: 35.7143rem;
+  .item {
+    padding: 1.4286rem 1.0714rem;
+    > div {
+      width: 100%;
+      height: 12.1429rem;
+      border: 1px solid #000;
+    }
+    > p {
+      font-size: 1.1429rem;
+      line-height: 1.4286rem;
     }
   }
 }
--- src/views/integration/integration.vue
+++ src/views/integration/integration.vue
@@ -1,16 +1,56 @@
 <template>
-  <div>
-    integration
-  </div>
+  <ul class="clearfix">
+    <li class="show-box clearfix">
+      <ul class="show clearfix">
+        <li>Led Colourful HD Display</li>
+        <li></li>
+        <li>Specifications: <span>200*300</span></li>
+        <li>
+          Curabitur auctor tristique lobortis. Quisque bibendum, ipsum in feugiat pharetra, odio libero malesuada turpis, tempus fermentum augue est sit amet magna. Vestibulum bibendum lectus non mauris porta, sed blandit purus scelerisque. Sed consequat mollis ornare. Sed laoreet id dolor vitae facilisis. Mauris varius orci sed turpis commodo mattis.
+        </li>
+      </ul>
+      <div class="blank"></div>
+      <ul class="operate clearfix">
+        <li></li>
+        <li></li>
+      </ul>
+      <div class="blank"></div>
+      <div class="related">
+        <related />
+      </div>
+    </li>
+    <li class="parms-table"></li>
+    <li class="submit"></li>
+  </ul>
 </template>
 
 <script>
-
+import related from '@/components/related/related';
 export default {
-  name: 'Integration'
-
+  name: 'Integration',
+  components: { related }
 };
 </script>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
+.show-box {
+  height: 800px;
+  > ul,
+  > div {
+    float: left;
+    height: 100%;
+  }
+  .blank {
+    width: 1.5%;
+  }
+  .show {
+    width: 21%;
+  }
+  .operate {
+    width: 57%;
+  }
+  .related {
+    width: 19%;
+  }
+}
 </style>
