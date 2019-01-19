2fe556b44fa96fe10c53458c7dc69472c5c87b67
Mario X Wang <wangxing_188@126.com>
2019-01-19 12:35:28 +0800

    删除多余文件

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,17 @@
 <template>
   <div id="app">
-    <el-menu :default-active="$route.path" class="el-menu-demo" mode="horizontal">
-      <el-menu-item index="1">处理中心</el-menu-item>
-      <el-submenu index="2">
-        <template slot="title">我的工作台</template>
-        <el-menu-item index="2-1">选项1</el-menu-item>
-        <el-menu-item index="2-2">选项2</el-menu-item>
-        <el-menu-item index="2-3">选项3</el-menu-item>
-        <el-submenu index="2-4">
-          <template slot="title">选项4</template>
-          <el-menu-item index="2-4-1">选项1</el-menu-item>
-          <el-menu-item index="2-4-2">选项2</el-menu-item>
-          <el-menu-item index="2-4-3">选项3</el-menu-item>
-        </el-submenu>
-      </el-submenu>
-      <el-menu-item index="3" disabled>消息中心</el-menu-item>
-      <el-menu-item index="4"><a href="https://www.ele.me" target="_blank">订单管理</a></el-menu-item>
-    </el-menu>
+    <ul class="menu clearfix">
+
+      <li v-for="link in routes" :key="link.path" class="menu-item">
+        <router-link v-if="!link.hidden" :to="link.path">
+          {{link.name}}
+        </router-link>
+      </li>
+    </ul>
+    <div class="content">
+      <router-view />
+    </div>
+    <div class="foot" />
   </div>
 </template>
 
@@ -29,8 +24,19 @@ export default {
       activeIndex2: '1'
     }
   },
-  routes() {
-    return this.$router.options.routes
+  computed: {
+    routes() {
+      return this.$router.options.routes
+    }
   }
 }
 </script>
+<style  rel="stylesheet/scss" lang="scss" scoped>
+.menu {
+  width: 100%;
+  display: flex;
+}
+.menu-item {
+  flex: 1;
+}
+</style>
