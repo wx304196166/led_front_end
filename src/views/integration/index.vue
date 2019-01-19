c8af393e03285973c0294f1729cd98ce5b29d125
Mario X Wang <wangxing_188@126.com>
2019-01-19 12:41:04 +0800

    constructor

---
+++ src/views/integration/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    integration
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Integration'
+
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+</style>
---
+++ src/views/products/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    products
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Products'
+
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+</style>
