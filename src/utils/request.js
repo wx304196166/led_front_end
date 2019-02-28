09cea7934b23f9c3f21a053a5c006f5164369a14
wx304196166 <13327468696@163.com>
2019-02-28 20:32:12 +0800

    更新

--- src/utils/request.js
+++ src/utils/request.js
@@ -9,7 +9,7 @@ import {
 // 创建axios实例
 const service = axios.create({
   baseURL: process.env.BASE_API, // api 的 base_url
-  timeout: 15000 // 请求超时时间
+  timeout: 900000 // 请求超时时间
 });
 
 // request拦截器
